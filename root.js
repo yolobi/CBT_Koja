b774f6849b9c53d5584c99e70f42f6967d86ca5d
L29 <linuztri@gmail.com>
2021-06-27 18:26:46 +0700

    123

--- root.js
+++
@@ -1,8 +0,0 @@
-Java.choose("com.chevaliers.secretmanager" , {
-  onMatch : function(instance){ //This function will be called for every instance found by frida
-    console.log("Found instance: "+instance);
-    console.log("Result of secret func: " + instance.secret());
-  },
-  onComplete:function(){}
-
-});
