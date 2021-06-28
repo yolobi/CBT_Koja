127b77c25d2c25178d7b62f4ec2d5bdd394aed52
L29 <linuztri@gmail.com>
2021-06-28 08:16:19 +0700

    fix

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 bcrypt = Bcrypt(app)
 app.config["JWT_COOKIE_SECURE"] = False
 app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
-app.config['JWT_SECRET_KEY'] = 'secret'
+app.config['JWT_SECRET_KEY'] = '_5#y2L"F4Q8z\n\xec]/'
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=3)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 jwt = JWTManager(app)
