2cca0d8e47f3a611bbc7592d9bd37e9b4305028a
L29 <linuztri@gmail.com>
2021-07-04 22:14:06 +0700

    add tele baru

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ import sys
 import mysql.connector
 
 def MysqlConnection():
-	return (mysql.connector.connect(host="mysql", user="root", password="PlantZo@123", auth_plugin='mysql_native_password', database="koja"))
+	return (mysql.connector.connect(host="172.17.0.1", user="root", password="PlantZo@123", auth_plugin='mysql_native_password', database="koja"))
 
 
 mysql = MysqlConnection()
