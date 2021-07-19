43f276b7de5acd8316ffe2b2e0bebbf54ac52a35
L29 <linuztri@gmail.com>
2021-07-19 13:24:08 +0700

    add page soal

--- admin.py
+++ admin.py
@@ -17,14 +17,33 @@ def add_soal():
 		data = dict(request.form)
 		bidang = data['bidang']
 		nomor = data['no_soal']
-		soal = data['soal']
+		soal = data['soal'].replace('\r\n', '')
 		opsi_a = data['opsi_a'].lower()
 		opsi_b = data['opsi_b'].lower()
 		opsi_c = data['opsi_c'].lower()
 		opsi_d = data['opsi_d'].lower()
 		opsi_e = data['opsi_e'].lower()
-		point = data['point']
+		point = data['bobot']
 		kj = data['kunci']
-		cur.execute("INSERT INTO %s (id, bidang, soal, opsi_A, opsi_B, opsi_C, opsi_D, opsi_E, point, kj) \
-		VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (bidang, nomor, soal, opsi_a, opsi_b, opsi_c, opsi_d, opsi_e, kj))
+		print(data)
+		if bidang == 'matematika':
+			cur.execute("INSERT INTO `matematika` (id, soal, opsi_A, opsi_B, opsi_C, opsi_D, opsi_E, point, kj) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", (nomor, soal, opsi_a, opsi_b, opsi_c, opsi_d, opsi_e, point, kj))
+		elif bidang == 'biologi':
+			cur.execute("INSERT INTO `biologi` (id, soal, opsi_A, opsi_B, opsi_C, opsi_D, opsi_E, point, kj) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", (nomor, soal, opsi_a, opsi_b, opsi_c, opsi_d, opsi_e, point, kj))
+		elif bidang == 'fisika':
+			cur.execute("INSERT INTO `fisika` (id, soal, opsi_A, opsi_B, opsi_C, opsi_D, opsi_E, point, kj) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", (nomor, soal, opsi_a, opsi_b, opsi_c, opsi_d, opsi_e, point, kj))
+		elif bidang == 'kimia':
+			cur.execute("INSERT INTO `kimia` (id, soal, opsi_A, opsi_B, opsi_C, opsi_D, opsi_E, point, kj) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", (nomor, soal, opsi_a, opsi_b, opsi_c, opsi_d, opsi_e, point, kj))
+		elif bidang == 'komputer':
+			cur.execute("INSERT INTO `komputer` (id, soal, opsi_A, opsi_B, opsi_C, opsi_D, opsi_E, point, kj) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", (nomor, soal, opsi_a, opsi_b, opsi_c, opsi_d, opsi_e, point, kj))
+		elif bidang == 'astronomi':
+			cur.execute("INSERT INTO `astronomi` (id, soal, opsi_A, opsi_B, opsi_C, opsi_D, opsi_E, point, kj) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", (nomor, soal, opsi_a, opsi_b, opsi_c, opsi_d, opsi_e, point, kj))
+		elif bidang == 'geografi':
+			cur.execute("INSERT INTO `geografi` (id, soal, opsi_A, opsi_B, opsi_C, opsi_D, opsi_E, point, kj) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", (nomor, soal, opsi_a, opsi_b, opsi_c, opsi_d, opsi_e, point, kj))
+		elif bidang == 'kebumian':
+			cur.execute("INSERT INTO `kebumian` (id, soal, opsi_A, opsi_B, opsi_C, opsi_D, opsi_E, point, kj) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", (nomor, soal, opsi_a, opsi_b, opsi_c, opsi_d, opsi_e, point, kj))
+		elif bidang == 'ekonomi':
+			cur.execute("INSERT INTO `ekonomi` (id, soal, opsi_A, opsi_B, opsi_C, opsi_D, opsi_E, point, kj) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", (nomor, soal, opsi_a, opsi_b, opsi_c, opsi_d, opsi_e, point, kj))
+		mysql.commit()
+		cur.close()
 		return '123'
