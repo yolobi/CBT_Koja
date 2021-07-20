f9a8e28657c62022ad376adeeab832aca73c54c6
L29 <linuztri@gmail.com>
2021-07-20 14:53:39 +0700

    add soal

--- soal.py
+++ soal.py
@@ -23,7 +23,9 @@ def komputer(id):
             for result in rv:
                 json_data.append(dict(zip(row_headers,result)))
             res = json.loads(json.dumps(json_data))[0]
-            if res['opsi_A'] == res['opsi_B'] == res['opsi_C'] == res['opsi_D'] == res['opsi_E'] == '' and res['kj'] != 'essai':
+            if res['kj'] == 'essay' or res['kj'] == 'essai':
+                return render_template("soal_essai.html", user=auth, bidang=res)
+            elif res['opsi_A'] == res['opsi_B'] == res['opsi_C'] == res['opsi_D'] == res['opsi_E'] == '':
                 return render_template("isian_singkat.html", user=auth, bidang=res)
             else:
                 return render_template("pilihan_ganda.html", user=auth, bidang=res)
@@ -32,3 +34,235 @@ def komputer(id):
     else:
         error = "You need to login first"
         return render_template("index.html", error=error)
+
+
+@app.route("/matematika/1c096d6e413c588e44cb9031d03b012f/<id>", methods=['GET'])
+def matematika(id):
+    if (request.method == 'GET' and request.cookies.get("auth")):
+        token = request.cookies.get('auth')
+        payload = jwt.decode(token, app.config.get('JWT_SECRET_KEY'), algorithms=['HS256'])
+        auth = payload['sub']
+        print(auth['bidang'])
+        if auth['bidang'] == 'matematika':
+            cur = mysql.cursor(buffered=True)
+            cur.execute("SELECT * FROM matematika where id = %s", (id,))
+            row_headers = [x[0] for x in cur.description]
+            rv = cur.fetchall()
+            json_data = []
+            for result in rv:
+                json_data.append(dict(zip(row_headers,result)))
+            res = json.loads(json.dumps(json_data))[0]
+            if res['kj'] == 'essay' or res['kj'] == 'essai':
+                return render_template("soal_essai.html", user=auth, bidang=res)
+            elif res['opsi_A'] == res['opsi_B'] == res['opsi_C'] == res['opsi_D'] == res['opsi_E'] == '':
+                return render_template("isian_singkat.html", user=auth, bidang=res)
+            else:
+                return render_template("pilihan_ganda.html", user=auth, bidang=res)
+        else:
+            return 'Bidang anda tidak sesuai'
+    else:
+        error = "You need to login first"
+        return render_template("index.html", error=error)
+
+
+@app.route("/biologi/1c096d6e413c588e44cb9031d03b012f/<id>", methods=['GET'])
+def matematika(id):
+    if (request.method == 'GET' and request.cookies.get("auth")):
+        token = request.cookies.get('auth')
+        payload = jwt.decode(token, app.config.get('JWT_SECRET_KEY'), algorithms=['HS256'])
+        auth = payload['sub']
+        print(auth['bidang'])
+        if auth['bidang'] == 'biologi':
+            cur = mysql.cursor(buffered=True)
+            cur.execute("SELECT * FROM biologi where id = %s", (id,))
+            row_headers = [x[0] for x in cur.description]
+            rv = cur.fetchall()
+            json_data = []
+            for result in rv:
+                json_data.append(dict(zip(row_headers,result)))
+            res = json.loads(json.dumps(json_data))[0]
+            if res['kj'] == 'essay' or res['kj'] == 'essai':
+                return render_template("soal_essai.html", user=auth, bidang=res)
+            elif res['opsi_A'] == res['opsi_B'] == res['opsi_C'] == res['opsi_D'] == res['opsi_E'] == '':
+                return render_template("isian_singkat.html", user=auth, bidang=res)
+            else:
+                return render_template("pilihan_ganda.html", user=auth, bidang=res)
+        else:
+            return 'Bidang anda tidak sesuai'
+    else:
+        error = "You need to login first"
+        return render_template("index.html", error=error)
+
+    
+@app.route("/kimia/1c096d6e413c588e44cb9031d03b012f/<id>", methods=['GET'])
+def kimia(id):
+    if (request.method == 'GET' and request.cookies.get("auth")):
+        token = request.cookies.get('auth')
+        payload = jwt.decode(token, app.config.get('JWT_SECRET_KEY'), algorithms=['HS256'])
+        auth = payload['sub']
+        print(auth['bidang'])
+        if auth['bidang'] == 'kimia':
+            cur = mysql.cursor(buffered=True)
+            cur.execute("SELECT * FROM kimia where id = %s", (id,))
+            row_headers = [x[0] for x in cur.description]
+            rv = cur.fetchall()
+            json_data = []
+            for result in rv:
+                json_data.append(dict(zip(row_headers,result)))
+            res = json.loads(json.dumps(json_data))[0]
+            if res['kj'] == 'essay' or res['kj'] == 'essai':
+                return render_template("soal_essai.html", user=auth, bidang=res)
+            elif res['opsi_A'] == res['opsi_B'] == res['opsi_C'] == res['opsi_D'] == res['opsi_E'] == '':
+                return render_template("isian_singkat.html", user=auth, bidang=res)
+            else:
+                return render_template("pilihan_ganda.html", user=auth, bidang=res)
+        else:
+            return 'Bidang anda tidak sesuai'
+    else:
+        error = "You need to login first"
+        return render_template("index.html", error=error)
+
+
+@app.route("/astronomi/1c096d6e413c588e44cb9031d03b012f/<id>", methods=['GET'])
+def astronomi(id):
+    if (request.method == 'GET' and request.cookies.get("auth")):
+        token = request.cookies.get('auth')
+        payload = jwt.decode(token, app.config.get('JWT_SECRET_KEY'), algorithms=['HS256'])
+        auth = payload['sub']
+        print(auth['bidang'])
+        if auth['bidang'] == 'astronomi':
+            cur = mysql.cursor(buffered=True)
+            cur.execute("SELECT * FROM astronomi where id = %s", (id,))
+            row_headers = [x[0] for x in cur.description]
+            rv = cur.fetchall()
+            json_data = []
+            for result in rv:
+                json_data.append(dict(zip(row_headers,result)))
+            res = json.loads(json.dumps(json_data))[0]
+            if res['kj'] == 'essay' or res['kj'] == 'essai':
+                return render_template("soal_essai.html", user=auth, bidang=res)
+            elif res['opsi_A'] == res['opsi_B'] == res['opsi_C'] == res['opsi_D'] == res['opsi_E'] == '':
+                return render_template("isian_singkat.html", user=auth, bidang=res)
+            else:
+                return render_template("pilihan_ganda.html", user=auth, bidang=res)
+        else:
+            return 'Bidang anda tidak sesuai'
+    else:
+        error = "You need to login first"
+        return render_template("index.html", error=error)
+
+
+@app.route("/fisika/1c096d6e413c588e44cb9031d03b012f/<id>", methods=['GET'])
+def fisika(id):
+    if (request.method == 'GET' and request.cookies.get("auth")):
+        token = request.cookies.get('auth')
+        payload = jwt.decode(token, app.config.get('JWT_SECRET_KEY'), algorithms=['HS256'])
+        auth = payload['sub']
+        print(auth['bidang'])
+        if auth['bidang'] == 'fisika':
+            cur = mysql.cursor(buffered=True)
+            cur.execute("SELECT * FROM fisika where id = %s", (id,))
+            row_headers = [x[0] for x in cur.description]
+            rv = cur.fetchall()
+            json_data = []
+            for result in rv:
+                json_data.append(dict(zip(row_headers,result)))
+            res = json.loads(json.dumps(json_data))[0]
+            if res['kj'] == 'essay' or res['kj'] == 'essai':
+                return render_template("soal_essai.html", user=auth, bidang=res)
+            elif res['opsi_A'] == res['opsi_B'] == res['opsi_C'] == res['opsi_D'] == res['opsi_E'] == '':
+                return render_template("isian_singkat.html", user=auth, bidang=res)
+            else:
+                return render_template("pilihan_ganda.html", user=auth, bidang=res)
+        else:
+            return 'Bidang anda tidak sesuai'
+    else:
+        error = "You need to login first"
+        return render_template("index.html", error=error)
+
+
+@app.route("/ekonomi/1c096d6e413c588e44cb9031d03b012f/<id>", methods=['GET'])
+def ekonomi(id):
+    if (request.method == 'GET' and request.cookies.get("auth")):
+        token = request.cookies.get('auth')
+        payload = jwt.decode(token, app.config.get('JWT_SECRET_KEY'), algorithms=['HS256'])
+        auth = payload['sub']
+        print(auth['bidang'])
+        if auth['bidang'] == 'ekonomi':
+            cur = mysql.cursor(buffered=True)
+            cur.execute("SELECT * FROM ekonomi where id = %s", (id,))
+            row_headers = [x[0] for x in cur.description]
+            rv = cur.fetchall()
+            json_data = []
+            for result in rv:
+                json_data.append(dict(zip(row_headers,result)))
+            res = json.loads(json.dumps(json_data))[0]
+            if res['kj'] == 'essay' or res['kj'] == 'essai':
+                return render_template("soal_essai.html", user=auth, bidang=res)
+            elif res['opsi_A'] == res['opsi_B'] == res['opsi_C'] == res['opsi_D'] == res['opsi_E'] == '':
+                return render_template("isian_singkat.html", user=auth, bidang=res)
+            else:
+                return render_template("pilihan_ganda.html", user=auth, bidang=res)
+        else:
+            return 'Bidang anda tidak sesuai'
+    else:
+        error = "You need to login first"
+        return render_template("index.html", error=error)
+
+@app.route("/geografi/1c096d6e413c588e44cb9031d03b012f/<id>", methods=['GET'])
+def geografi(id):
+    if (request.method == 'GET' and request.cookies.get("auth")):
+        token = request.cookies.get('auth')
+        payload = jwt.decode(token, app.config.get('JWT_SECRET_KEY'), algorithms=['HS256'])
+        auth = payload['sub']
+        print(auth['bidang'])
+        if auth['bidang'] == 'geografi':
+            cur = mysql.cursor(buffered=True)
+            cur.execute("SELECT * FROM geografi where id = %s", (id,))
+            row_headers = [x[0] for x in cur.description]
+            rv = cur.fetchall()
+            json_data = []
+            for result in rv:
+                json_data.append(dict(zip(row_headers,result)))
+            res = json.loads(json.dumps(json_data))[0]
+            if res['kj'] == 'essay' or res['kj'] == 'essai':
+                return render_template("soal_essai.html", user=auth, bidang=res)
+            elif res['opsi_A'] == res['opsi_B'] == res['opsi_C'] == res['opsi_D'] == res['opsi_E'] == '':
+                return render_template("isian_singkat.html", user=auth, bidang=res)
+            else:
+                return render_template("pilihan_ganda.html", user=auth, bidang=res)
+        else:
+            return 'Bidang anda tidak sesuai'
+    else:
+        error = "You need to login first"
+        return render_template("index.html", error=error)
+
+
+@app.route("/kebumian/1c096d6e413c588e44cb9031d03b012f/<id>", methods=['GET'])
+def kebumian(id):
+    if (request.method == 'GET' and request.cookies.get("auth")):
+        token = request.cookies.get('auth')
+        payload = jwt.decode(token, app.config.get('JWT_SECRET_KEY'), algorithms=['HS256'])
+        auth = payload['sub']
+        print(auth['bidang'])
+        if auth['bidang'] == 'kebumian':
+            cur = mysql.cursor(buffered=True)
+            cur.execute("SELECT * FROM kebumian where id = %s", (id,))
+            row_headers = [x[0] for x in cur.description]
+            rv = cur.fetchall()
+            json_data = []
+            for result in rv:
+                json_data.append(dict(zip(row_headers,result)))
+            res = json.loads(json.dumps(json_data))[0]
+            if res['kj'] == 'essay' or res['kj'] == 'essai':
+                return render_template("soal_essai.html", user=auth, bidang=res)
+            elif res['opsi_A'] == res['opsi_B'] == res['opsi_C'] == res['opsi_D'] == res['opsi_E'] == '':
+                return render_template("isian_singkat.html", user=auth, bidang=res)
+            else:
+                return render_template("pilihan_ganda.html", user=auth, bidang=res)
+        else:
+            return 'Bidang anda tidak sesuai'
+    else:
+        error = "You need to login first"
+        return render_template("index.html", error=error)
+
