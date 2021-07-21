cc1a05c9bbd7ad1bfdabd56b976176fb890af70d
yolobi <dimdimdimas@apps.ipb.ac.id>
2021-07-21 08:21:17 +0700

    new next prev

--- static/js/pindah.js
+++ static/js/pindah.js
@@ -1,30 +1,37 @@
-function pindah(bidang_id, len) {
+function next(bidang_id, len) {
   "use strict";
   var page = bidang_id;
   var pageCount = len;
 
-  document.getElementById("btnPrev").onclick = function() {
-    page = ((page + pageCount + 1) % pageCount) + 1;
-    window.location.href='http://localhost/komputer/1c096d6e413c588e44cb9031d03b012f/'+page;
-  };
-  document.getElementById("btnNext").onclick = function() {
-    page = (page % pageCount) + 1;
-    window.location.href='http://localhost/komputer/1c096d6e413c588e44cb9031d03b012f/'+page;
-  };
-
-};
-
-function navigasi(bidang_id){
-  "use strict";
-  var page = bidang_id;
-  console.log(page);
+  page = ((page + pageCount + 1) % pageCount) + 1;
   window.location.href='http://localhost/komputer/1c096d6e413c588e44cb9031d03b012f/'+page;
 
 };
 
-function essai(){
+function prev(bidang_id, len) {
+  "use strict"
+  var page = bidang_id;
+  var pageCount = len;
+  page = (page % pageCount) + 1;
+  window.location.href='http://localhost/komputer/1c096d6e413c588e44cb9031d03b012f/'+page;
+}
+
+function navigasi(bidang_id){
+  "use strict";
+  var page = bidang_id;
+  window.location.href='http://localhost/komputer/1c096d6e413c588e44cb9031d03b012f/'+page;
+
+};
+
+function essai(bidang){
   "use strict";
   document.getElementById("essai").onclick = function(){
   window.location.href='http://localhost/komputer/1c096d6e413c588e44cb9031d03b012f/essai';
   };
 };
+
+function clear(){
+  var ele = document.getElementsByName("opsi");
+  for(var i=0;i<ele.length;i++)
+      ele[i].checked = false;
+}
