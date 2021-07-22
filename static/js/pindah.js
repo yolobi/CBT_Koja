ad8c334626bb50f26c24e86daf5958c04760cf75
yolobi <dimdimdimas@apps.ipb.ac.id>
2021-07-22 19:35:08 +0700

    koja-olimpiade

--- static/js/pindah.js
+++ static/js/pindah.js
@@ -4,7 +4,7 @@ function next(bidang_id, len, bidang) {
   var pageCount = len;
 
   page = ((page + pageCount + 1) % pageCount) + 1;
-  window.location.href='http://localhost/'+bidang+'/1c096d6e413c588e44cb9031d03b012f/'+page;
+  window.location.href='http://koja-olimpiade.com/'+bidang+'/1c096d6e413c588e44cb9031d03b012f/'+page;
 
 };
 
@@ -13,22 +13,22 @@ function prev(bidang_id, len, bidang) {
   var page = bidang_id;
   var pageCount = len;
   page = (page % pageCount) + 1;
-  window.location.href='http://localhost/'+bidang+'/1c096d6e413c588e44cb9031d03b012f/'+page;
+  window.location.href='http://koja-olimpiade.com/'+bidang+'/1c096d6e413c588e44cb9031d03b012f/'+page;
 }
 
 function navigasi(bidang_id, bidang){
   "use strict";
   var page = bidang_id;
-  window.location.href='http://localhost/'+bidang+'/1c096d6e413c588e44cb9031d03b012f/'+page;
+  window.location.href='http://koja-olimpiade.com/'+bidang+'/1c096d6e413c588e44cb9031d03b012f/'+page;
 
 };
 
 function essai(bidang){
   "use strict";
-  window.location.href='http://localhost/'+bidang+'/essay';
+  window.location.href='http://koja-olimpiade.com/'+bidang+'/essay';
 };
 
 function selesai(bidang){
   "use strict"
-  window.location.href='http://localhost/'+bidang+'/finish';
+  window.location.href='http://koja-olimpiade.com/'+bidang+'/finish';
 }
