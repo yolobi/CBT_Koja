99893d78fe33fa2e871f9eefb23d78066c2bcd2c
L29 <linuztri@gmail.com>
2021-07-23 13:22:11 +0700

    ujicoba

--- soal.py
+++ soal.py
@@ -32,7 +32,7 @@ def decode_jwt(token):
     return res
 
 
-@app.route("/start/1c096d6e413c588e44cb9031d03b012f")
+@app.route("/start/xxx")
 def start():
     cur = mysql.cursor(buffered=True)
     id = 1
@@ -54,7 +54,63 @@ def start():
     print(session)
     return response
 
-@app.route("/komputer/1c096d6e413c588e44cb9031d03b012f/<id>", methods=['GET'])
+@app.route("/ujicoba")
+def uji():
+    cur = mysql.cursor(buffered=True)
+    id = 1
+    token = request.cookies.get('auth')
+    payload = decode_jwt(token)
+    auth = payload['sub']
+    #check session
+    cur = mysql.cursor(buffered=True)
+    cur.execute("select session from users where uid = %s", (auth['uid'],))
+    rv = cur.fetchone()
+    print(rv)
+    if (rv[0]):
+        return 'Session anda telah habis'
+    if (request.cookies.get("session")):
+        return redirect(url_for('{}'.format(auth['bidang']),id=id))
+    response = make_response(redirect(url_for('coba', id=id)))
+    session = create_timer(auth['uid'])
+    response.set_cookie('session', session)
+    print(session)
+    return response
+
+@app.route("/ujicoba_tryout/<id>", methods=['GET'])
+@cache.cached(timeout=10, query_string=True)
+def coba(id):
+    if (request.method == 'GET' and request.cookies.get("auth") and request.cookies.get("session")):
+        token = request.cookies.get('auth')
+        session = request.cookies.get('session')
+        payload = decode_jwt(token)
+        auth = payload['sub']
+        try:
+            timer = decode_jwt(session)
+        except:
+            return redirect(url_for('upload_essay',bidang=auth['bidang']))
+        if (auth['bidang']):
+            cur = mysql.cursor(buffered=True)
+            total = total_soal[auth['bidang']]
+            cur.execute("SELECT * FROM komputer where id = %s", (id,))
+            row_headers = [x[0] for x in cur.description]
+            rv = cur.fetchall()
+            json_data = []
+            for result in rv:
+                json_data.append(dict(zip(row_headers,result)))
+            res = json.loads(json.dumps(json_data))[0]
+            if res['kj'] == 'essay' or res['kj'] == 'essai':
+                return render_template("soal_essai.html", user=auth, bidang=res, len=total, timer=timer['exp']*1000)
+            elif res['opsi_A'] == res['opsi_B'] == res['opsi_C'] == res['opsi_D'] == res['opsi_E'] == '':
+                return render_template("isian_singkat.html", user=auth, bidang=res, len=total, timer=timer['exp']*1000)
+            else:
+                return render_template("pilihan_ganda.html", user=auth, bidang=res, len=total, timer=timer['exp']*1000)
+        else:
+            return 'Bidang anda tidak sesuai'
+    else:
+        error = "You need to login first"
+        return render_template("index.html", error=error)
+
+@app.route("/komputer/xxx/<id>", methods=['GET'])
 @cache.cached(timeout=10, query_string=True)
 def komputer(id):
     if (request.method == 'GET' and request.cookies.get("auth") and request.cookies.get("session")):
@@ -65,7 +121,7 @@ def komputer(id):
         try:
             timer = decode_jwt(session)
         except:
-            return redirect(url_for('finish_attempt',bidang=auth['bidang']))
+            return redirect(url_for('upload_essay',bidang=auth['bidang']))
         if auth['bidang'] == 'komputer':
             cur = mysql.cursor(buffered=True)
             total = total_soal[auth['bidang']]
@@ -89,7 +145,7 @@ def komputer(id):
         return render_template("index.html", error=error)
 
 
-@app.route("/matematika/1c096d6e413c588e44cb9031d03b012f/<id>", methods=['GET'])
+@app.route("/matematika/xxx/<id>", methods=['GET'])
 @cache.cached(timeout=10, query_string=True)
 def matematika(id):
     if (request.method == 'GET' and request.cookies.get("auth") and request.cookies.get("session")):
@@ -100,7 +156,7 @@ def matematika(id):
         try:
             timer = decode_jwt(session)
         except:
-            return redirect(url_for('finish_attempt',bidang=auth['bidang']))
+            return redirect(url_for('upload_essay',bidang=auth['bidang']))
         if auth['bidang'] == 'matematika':
             cur = mysql.cursor(buffered=True)
             total = total_soal[auth['bidang']]
@@ -125,7 +181,7 @@ def matematika(id):
         return redirect(url_for('home'))
 
 
-@app.route("/biologi/1c096d6e413c588e44cb9031d03b012f/<id>", methods=['GET'])
+@app.route("/biologi/xxx/<id>", methods=['GET'])
 @cache.cached(timeout=10, query_string=True)
 def biologi(id):
     if (request.method == 'GET' and request.cookies.get("auth") and request.cookies.get("session")):
@@ -136,7 +192,7 @@ def biologi(id):
         try:
             timer = decode_jwt(session)
         except:
-            return redirect(url_for('finish_attempt',bidang=auth['bidang']))
+            return redirect(url_for('upload_essay',bidang=auth['bidang']))
         if auth['bidang'] == 'biologi':
             cur = mysql.cursor(buffered=True)
             total = total_soal[auth['bidang']]
@@ -159,7 +215,7 @@ def biologi(id):
         error = "You need to login first"
         return render_template("index.html", error=error)
     
-@app.route("/kimia/1c096d6e413c588e44cb9031d03b012f/<id>", methods=['GET'])
+@app.route("/kimia/xxx/<id>", methods=['GET'])
 @cache.cached(timeout=10, query_string=True)
 def kimia(id):
     if (request.method == 'GET' and request.cookies.get("auth") and request.cookies.get("session")):
@@ -170,7 +226,7 @@ def kimia(id):
         try:
             timer = decode_jwt(session)
         except:
-            return redirect(url_for('finish_attempt',bidang=auth['bidang']))
+            return redirect(url_for('upload_essay',bidang=auth['bidang']))
         if auth['bidang'] == 'kimia':
             cur = mysql.cursor(buffered=True)
             total = total_soal[auth['bidang']]
@@ -194,7 +250,7 @@ def kimia(id):
         return render_template("index.html", error=error)
 
 
-@app.route("/astronomi/1c096d6e413c588e44cb9031d03b012f/<id>", methods=['GET'])
+@app.route("/astronomi/xxx/<id>", methods=['GET'])
 @cache.cached(timeout=10, query_string=True)
 def astronomi(id):
     if (request.method == 'GET' and request.cookies.get("auth") and request.cookies.get("session")):
@@ -205,7 +261,7 @@ def astronomi(id):
         try:
             timer = decode_jwt(session)
         except:
-            return redirect(url_for('finish_attempt',bidang=auth['bidang']))
+            return redirect(url_for('upload_essay',bidang=auth['bidang']))
         if auth['bidang'] == 'astronomi':
             cur = mysql.cursor(buffered=True)
             total = total_soal[auth['bidang']]
@@ -229,7 +285,7 @@ def astronomi(id):
         return render_template("index.html", error=error)
 
 
-@app.route("/fisika/1c096d6e413c588e44cb9031d03b012f/<id>", methods=['GET'])
+@app.route("/fisika/xxx/<id>", methods=['GET'])
 @cache.cached(timeout=10, query_string=True)
 def fisika(id):
     if (request.method == 'GET' and request.cookies.get("auth") and request.cookies.get("session")):
@@ -240,7 +296,7 @@ def fisika(id):
         try:
             timer = decode_jwt(session)
         except:
-            return redirect(url_for('finish_attempt',bidang=auth['bidang']))
+            return redirect(url_for('upload_essay',bidang=auth['bidang']))
         if auth['bidang'] == 'fisika':
             cur = mysql.cursor(buffered=True)
             total = total_soal[auth['bidang']]
@@ -264,7 +320,7 @@ def fisika(id):
         return render_template("index.html", error=error)
 
 
-@app.route("/ekonomi/1c096d6e413c588e44cb9031d03b012f/<id>", methods=['GET'])
+@app.route("/ekonomi/xxx/<id>", methods=['GET'])
 @cache.cached(timeout=10, query_string=True)
 def ekonomi(id):
     if (request.method == 'GET' and request.cookies.get("auth") and request.cookies.get("session")):
@@ -275,7 +331,7 @@ def ekonomi(id):
         try:
             timer = decode_jwt(session)
         except:
-            return redirect(url_for('finish_attempt',bidang=auth['bidang']))
+            return redirect(url_for('upload_essay',bidang=auth['bidang']))
         if auth['bidang'] == 'ekonomi':
             cur = mysql.cursor(buffered=True)
             total = total_soal[auth['bidang']]
@@ -298,7 +354,7 @@ def ekonomi(id):
         error = "You need to login first"
         return render_template("index.html", error=error)
 
-@app.route("/geografi/1c096d6e413c588e44cb9031d03b012f/<id>", methods=['GET'])
+@app.route("/geografi/xxx/<id>", methods=['GET'])
 @cache.cached(timeout=10, query_string=True)
 def geografi(id):
     if (request.method == 'GET' and request.cookies.get("auth") and request.cookies.get("session")):
@@ -309,7 +365,7 @@ def geografi(id):
         try:
             timer = decode_jwt(session)
         except:
-            return redirect(url_for('finish_attempt',bidang=auth['bidang']))
+            return redirect(url_for('upload_essay',bidang=auth['bidang']))
         if auth['bidang'] == 'geografi':
             cur = mysql.cursor(buffered=True)
             total = total_soal[auth['bidang']]
@@ -333,7 +389,7 @@ def geografi(id):
         return render_template("index.html", error=error)
 
 
-@app.route("/kebumian/1c096d6e413c588e44cb9031d03b012f/<id>", methods=['GET'])
+@app.route("/kebumian/xxx/<id>", methods=['GET'])
 @cache.cached(timeout=10, query_string=True)
 def kebumian(id):
     if (request.method == 'GET' and request.cookies.get("auth") and request.cookies.get("session")):
@@ -344,7 +400,7 @@ def kebumian(id):
         try:
             timer = decode_jwt(session)
         except:
-            return redirect(url_for('finish_attempt',bidang=auth['bidang']))
+            return redirect(url_for('upload_essay',bidang=auth['bidang']))
         if auth['bidang'] == 'kebumian':
             cur = mysql.cursor(buffered=True)
             total = total_soal[auth['bidang']]
@@ -415,3 +471,31 @@ def finish_post():
         resp = make_response(redirect(url_for("home")))
         resp.set_cookie('session', '', expires=0)
         return resp
+
+@app.route("/api/essay", methods=['POST'])
+def api_essay():
+	ALLOWED_EXTENSIONS = set(['pdf'])
+
+	def allowed_file(filename):
+		return '.' in filename and \
+			filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+	if request.method == 'POST':
+		token = request.cookies.get('auth')
+		payload = jwt.decode(token, app.config.get('JWT_SECRET_KEY'), algorithms=['HS256'])
+		auth = payload['sub']
+		dir =  'essay/{}'.format(str(auth['uid'])+'_'+auth['name'])
+		if os.path.exists(dir):
+			shutil.rmtree(dir)
+		os.makedirs(dir)
+		UPLOAD_FOLDER = 'essay/{}'.format(str(auth['uid'])+'_'+auth['name'])
+		ALLOWED_EXTENSIONS = set(['pdf'])
+		app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+		ess = request.files['ess']
+		if allowed_file(ess.filename):
+			ess_name = secure_filename(ess.filename)
+			ess.save(os.path.join(app.config['UPLOAD_FOLDER'], ess_name))
+			return redirect(url_for("finish_attempt", bidang=auth['bidang']))
+		else:
+			flash("File must be pdf")
+			return redirect(url_for("upload_essay", bidang=auth['bidang']))
