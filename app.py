a36e828e8b7d0b15f2663ac3c0800a9d160ce9c9
L29 <linuztri@gmail.com>
2021-07-25 11:20:57 +0700

    123

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ bcrypt = Bcrypt(app)
 app.config["JWT_COOKIE_SECURE"] = False
 app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
 app.config['JWT_SECRET_KEY'] = '_5#y2L"F4Q8z\n\xec]/'
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=3)
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=5)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 #mail
